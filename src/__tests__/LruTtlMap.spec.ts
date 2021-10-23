36c4ba15d13234bde8f2b4aa62b19fc3ebfab0bb
streamich <streamich@gmail.com>
2021-10-24 01:00:51 +0200

    feat: 🎸 place LruMap and LruTtlMap into separate files

--- src/__tests__/LruTtlMap.spec.ts
+++ src/__tests__/LruTtlMap.spec.ts
@@ -1,8 +1,8 @@
-import {LruMap} from '../LruMap';
+import {LruTtlMap} from '../LruTtlMap';
 
-describe('LruMap', () => {
+describe('LruTtlMap', () => {
   test('.get() / .set() / .size', () => {
-    const lru = new LruMap(123);
+    const lru = new LruTtlMap(123);
     lru.set('foo', 1);
     expect(lru.size).toBe(1);
     lru.set('bar', 2);
@@ -12,7 +12,7 @@ describe('LruMap', () => {
   });
 
   test('keeps limit', () => {
-    const lru = new LruMap(2);
+    const lru = new LruTtlMap(2);
     lru.set('1', 1);
     expect(lru.size).toBe(1);
     lru.set('2', 2);
@@ -28,7 +28,7 @@ describe('LruMap', () => {
   });
 
   test('rotates keys on .get()', () => {
-    const lru = new LruMap(3);
+    const lru = new LruTtlMap(3);
     lru.set('1', 1);
     expect(lru.size).toBe(1);
     lru.set('2', 2);
@@ -43,7 +43,7 @@ describe('LruMap', () => {
   });
 
   test('rotates keys on .set()', () => {
-    const lru = new LruMap(3);
+    const lru = new LruTtlMap(3);
     lru.set('1', 1);
     expect(lru.size).toBe(1);
     lru.set('2', 2);
@@ -57,8 +57,8 @@ describe('LruMap', () => {
     expect(lru.get('2')).toBe(2);
   });
 
-  test('should drop key least used on max size', () => {
-    const cache = new LruMap(3);
+  test('Should drop key least used on max size', () => {
+    const cache = new LruTtlMap(3);
     cache.set('a', '1');
     cache.set('b', '2');
     cache.set('c', '3');
@@ -76,7 +76,7 @@ describe('LruMap', () => {
   });
 
   test('should remove all objects on reset', () => {
-    const cache = new LruMap(3);
+    const cache = new LruTtlMap(3);
     cache.set('a', '1');
     cache.set('b', '2');
     cache.set('c', '3');
@@ -85,5 +85,41 @@ describe('LruMap', () => {
     cache.clear();
     expect(cache.size).toBe(0);
     expect([...cache.keys()].length).toBe(0);
+  });
+
+  test('returns a key that have not expired', () => {
+    const lru = new LruTtlMap(3);
+    lru.set('1', 1);
+    expect(lru.get('1')).toBe(1);
+    lru.set('2', 2, 10);
+    expect(lru.get('2', 9)).toBe(2);
+  });
+
+  test('does not return a key that has expired', () => {
+    const lru = new LruTtlMap(3);
+    lru.set('1', 1);
+    expect(lru.get('1', 123)).toBe(1);
+    lru.set('2', 2, 10);
+    expect(lru.get('2', 11)).toBe(undefined);
+  });
+
+  test('when item accessed multiple times it keeps its expiry time', () => {
+    const lru = new LruTtlMap(3);
+    lru.set('1', 1);
+    lru.set('2', 2, 100);
+    lru.set('3', 3);
+    expect(lru.get('2', 50)).toBe(2);
+    lru.set('4', 4);
+    expect(lru.get('2', 60)).toBe(2);
+    lru.set('5', 5);
+    expect(lru.get('2', 70)).toBe(2);
+    lru.set('6', 6);
+    expect(lru.get('2', 80)).toBe(2);
+    expect(lru.size).toBe(3);
+    expect(lru.get('5')).toBe(5);
+    expect(lru.get('6')).toBe(6);
+    expect(lru.size).toBe(3);
+    expect(lru.get('2', 120)).toBe(undefined);
+    expect(lru.size).toBe(2);
   });
 });
