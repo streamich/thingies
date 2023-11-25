4a7e5460362fe929dc8ccb1ac35cc5f3d22a6469
streamich <streamich@gmail.com>
2023-11-25 15:27:22 +0100

    fix: 🐛 make FanOut listeners public

--- src/fanout.ts
+++ src/fanout.ts
@@ -2,7 +2,7 @@ export type FanOutUnsubscribe = () => void;
 export type FanOutListener<D> = (data: D) => void;
 
 export class FanOut<D> {
-  protected readonly listeners = new Set<FanOutListener<D>>();
+  public readonly listeners = new Set<FanOutListener<D>>();
 
   public emit(data: D): void {
     this.listeners.forEach(listener => listener(data));
