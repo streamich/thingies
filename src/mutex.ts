c9942801490a62c547b34a881829e8c65322ffa8
streamich <streamich@gmail.com>
2024-03-19 09:46:03 +0100

    fix: 🐛 limit mutex scope to a single class instance

--- src/mutex.ts
+++ src/mutex.ts
@@ -5,11 +5,21 @@ import {codeMutex} from './codeMutex';
  * returns the result of the ongoing execution.
  */
 export function mutex<This, Args extends any[], Return>(
-  target: (this: This, ...args: Args) => Promise<Return>,
+  fn: (this: This, ...args: Args) => Promise<Return>,
   context?: ClassMethodDecoratorContext<This, (this: This, ...args: Args) => Promise<Return>>,
 ) {
-  const mut = codeMutex<Return>();
+  const isDecorator = !!context;
+  if (!isDecorator) {
+    const mut = codeMutex<Return>();
+    return async function (this: This, ...args: Args): Promise<Return> {
+      return await mut(async () => await fn.call(this, ...args));
+    };
+  }
+  const instances = new WeakMap<any, WeakMap<any, any>>();
   return async function (this: This, ...args: Args): Promise<Return> {
-    return await mut(async () => await target.call(this, ...args));
+    let map = instances.get(this);
+    if (!map) instances.set(this, map = new WeakMap<any, any>());
+    if (!map.has(fn)) map.set(fn, codeMutex<Return>());
+    return await map.get(fn)!(async () => await fn.call(this, ...args));
   };
 }
