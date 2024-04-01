5645f4e208e02013df9773a07ba07af2aedc13a0
streamich <streamich@gmail.com>
2024-04-01 18:58:31 +0200

    test: 💍 add more lock tests

--- src/__tests__/Locks.spec.ts
+++ src/__tests__/Locks.spec.ts
@@ -48,9 +48,9 @@ describe('.acquire()', () => {
     const unlock1 = locks.acquire('my-lock', 20);
     expect(!!unlock1).toBe(true);
     expect(locks.isLocked('my-lock')).toBe(true);
-    await tick(21);
+    await tick(25);
     expect(locks.isLocked('my-lock')).toBe(false);
-    const unlock2 = locks.acquire('my-lock', 1);
+    const unlock2 = locks.acquire('my-lock', 10);
     expect(!!unlock2).toBe(true);
     expect(locks.isLocked('my-lock')).toBe(true);
     unlock1!();
@@ -59,5 +59,65 @@ describe('.acquire()', () => {
     expect(locks.isLocked('my-lock')).toBe(true);
     unlock2!();
     expect(locks.isLocked('my-lock')).toBe(false);
+  });
+});
+
+describe('.lock()', () => {
+  test('should acquire lock', async () => {
+    const locks = new Locks({});
+    const fn = jest.fn();
+    await locks.lock('my-lock')(async () => {
+      fn();
+    });
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+
+  test('should acquire lock with timeout', async () => {
+    const locks = new Locks({});
+    const fn = jest.fn();
+    locks.lock('my-lock', 50)(async () => {
+      await tick(50);
+    });
+    await locks.lock('my-lock', 50, 100)(async () => {
+      fn();
+    });
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+
+  test('should throw when lock was not acquired within the specified timeout', async () => {
+    const locks = new Locks({});
+    locks.lock('my-lock', 10)(async () => {
+      await tick(10);
+    });
+    try {
+      await locks.lock('my-lock', 10, 5)(async () => {});
+      throw new Error('Not this');
+    } catch (e) {
+      expect(e).toStrictEqual(new Error('LOCK_TIMEOUT'));
+    }
+  });
+
+  test('should execute all code which can acquire the lock on time', async () => {
+    const locks = new Locks({});
+    const items: number[] = [];
+    await Promise.all([
+      locks.lock('my-lock', 50)(async () => {
+        await tick(5);
+        items.push(1);
+      }),
+      locks.lock('my-lock', 50, 200)(async () => {
+        await tick(5);
+        items.push(2);
+      }),
+      locks.lock('my-lock', 50, 200)(async () => {
+        await tick(5);
+        items.push(3);
+      }),
+      locks.lock('my-lock', 50, 200)(async () => {
+        await tick(5);
+        items.push(4);
+      }),
+    ]);
+    expect(items).toStrictEqual([1, 2, 3, 4]);
   });
 });
