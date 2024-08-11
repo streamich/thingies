4dd435c96f1e01e48231a92f943e3d2d89639740
streamich <streamich@gmail.com>
2024-08-11 10:53:13 +0200

    feat: 🎸 improve performance in key retrieval

--- src/LruMap.ts
+++ src/LruMap.ts
@@ -13,10 +13,11 @@ export class LruMap<K, V> extends Map<K, V> {
   public get(key: K): V | undefined {
     const value = super.get(key)!;
     if (value === void 0) {
-      if (super.has(key)) {
-        super.delete(key);
-        super.set(key, value);
-      }
+      const deleted = super.delete(key);
+      if (deleted) super.set(key, value);
+      // if (super.has(key)) {
+      //   super.set(key, value);
+      // }
       return;
     }
     super.delete(key);
